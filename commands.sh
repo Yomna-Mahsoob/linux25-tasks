4a1d11d198705b461fb33f9422a1fb5f0458ea7d
Yomna-Mahsoob <ymahsoob@gmail.com>
2025-02-06 15:44:03 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -5,5 +5,6 @@ mkdir task3
 cd task3
 touch commands.sh
 git add commands.sh
-git commit -m "Added task3"
+git commit -m "Added task3"g
+it tag -a V1 -m "Tagging the first version"
 git push origin main 
