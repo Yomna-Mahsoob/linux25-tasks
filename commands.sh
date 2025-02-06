a748f005dd2a34bc561b52f845d0886a4aa52c01
Yomna-Mahsoob <ymahsoob@gmail.com>
2025-02-06 15:38:24 +0200

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,9 @@
+mkdir linux25-tasks
+cd linux25-tasks
+git init .  # Initializes a Git repository.
+mkdir task3
+cd task3
+touch commands.sh
+git add commands.sh
+git commit -m "Added task3"
+git push origin main 
