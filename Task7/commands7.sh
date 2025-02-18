7c15f4b0f9222986a4d3a18ab6eb901e10a212b2
Yomna-Mahsoob <ymahsoob@gmail.com>
2025-02-18 12:16:57 +0200

    add task 7 solution

---
+++ Task7/commands7.sh
@@ -0,0 +1,12 @@
+# for keys
+sort keys | uniq -d > extracted_keys.txt 
+
+
+
+
+#for IP
+grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' data.txt
+#for phone numbers
+grep -oE '\+?[0-9]{10,15}' data.txt
+#for URLs
+grep -oE '(https?://|www\.)[a-zA-Z0-9.-]+' data.txt
